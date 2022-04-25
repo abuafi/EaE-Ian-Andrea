af0e39240b9a3ecb39b91d935bcd7f54af17ece9
Ian Abuaf <abuafi@usi.ch>
2022-04-25 16:08:20 +0200

    csv-parse module

--- Ex2/web_app/routes/index.js
+++ Ex2/web_app/routes/index.js
@@ -1,9 +1,15 @@
+var parse = require('csv-parse')
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Experiment 2' });
+});
+
+/* GET testing page. */
+router.get('/run', function(req, res, next) {
+  res.render('experimentview.ejs', { words: ['hello', 'world'] });
 });
 
 module.exports = router;
