08fad5682fb9b0152ff1efdf1c6f92e959b7367b
AndreaGualandris <andrea.gualandris@usi.ch>
2022-04-25 16:10:58 +0200

    Home page

--- Ex2/web_app/routes/index.js
+++ Ex2/web_app/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Experiment 2' });
+  // res.render('index', { title: 'Experiment 2' });
+  res.render('../views/index.ejs');
 });
 
 /* GET testing page. */
