492274e9baccd38db6addec15f70f9d4f1c8b455
Ian Abuaf <abuafi@usi.ch>
2022-05-01 23:26:39 +0200

    Added local and server storage

--- Ex2/web_app/routes/index.js
+++ Ex2/web_app/routes/index.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 // var fetch = require('node-fetch')
 var express = require('express');
+const { assert } = require('console');
 var router = express.Router();
 
 /* GET home page. */
@@ -13,5 +14,23 @@ router.get('/', function(req, res, next) {
 router.get('/run',async function(req, res, next) {
   res.render('experimentview.ejs');
 });
+
+const data_keys = ["correct","time","style","correctI"]
+const data_path = __dirname + "/../public/data.csv"
+router.patch('/data',function(req, res, next) {
+  req.on('data', (d) => {
+    let data = JSON.parse(d)
+    let f = ''
+    for (let line of data) {
+      for (let key of data_keys) {
+        f += line[key] + ","
+      }
+      f += '\n'
+    }
+    fs.appendFile(data_path, f, () => {
+      res.status(200).end()
+    })
+  })
+})
 
 module.exports = router;
