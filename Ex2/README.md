e7b1fb92549491c23b50f28ea1695dc26cae9fe4
Ian Abuaf <abuafi@usi.ch>
2022-05-16 01:00:18 +0200

    Final commit of Ex2

---
+++ Ex2/README.md
@@ -0,0 +1,21 @@
+# EaE-Ian-Andrea
+
+GitHub Page link:
+https://github.com/abuafi/EaE-Ian-Andrea/tree/main/Ex2
+
+Google Drive document link:
+https://docs.google.com/document/d/1gg6gkCZFf0X4YztR-iQ0S1lFMY_lutpsp7xKj_GmeKA/edit?usp=sharing
+
+The python script requires the Seaborn, Pandas and Matplotlib libraries.
+
+## Build Instructions:
+To host the experiment app on localhost:
+
+- `cd web_app`
+- `npm install`
+- `npm start`
+- Access from `http://localhost:8080`
+
+To run the data analysis (Create plots):
+
+- Run with `python3 visuals.py`
--- Ex2/visuals.py
+++ Ex2/visuals.py
@@ -3,7 +3,6 @@ import pandas
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import scipy
 dataframe = pandas.read_csv('web_app/public/final_data.csv')
 dataframe["uid"] = np.char.mod('%d', dataframe["uid"])
 
@@ -11,80 +10,80 @@ df_correct = dataframe.query("correct")
 df_no_outliers = dataframe.query("time <= 20000")
 df_correct_no_outliers = df_correct.query("time <= 20000")
 
-# sns.histplot(data=df_no_outliers, x="time")
-# plt.title("Distribution of the times recorded (< 20s)")
-# plt.savefig('./figures/Figure_0.png')
-# plt.close()
-# sns.histplot(data=dataframe.query('csexp == "yes"'), x="uid", hue="correct", discrete=True, multiple="stack", palette={True:'green',False:'red'})
-# sns.histplot(data=dataframe.query('csexp == "no"'), x="uid", hue="correct", discrete=True, multiple="stack", palette={True:'lime',False:'red'})
-# plt.title("Correct and incorrect answers per participant (Green have CS experience)")
-# plt.savefig('./figures/Figure_1.png')
-# plt.close()
-# sns.set(rc={"figure.figsize":(20, 4)})
-# sns.histplot(data=dataframe, y="correctS", hue="correct", discrete=True, multiple="stack")
-# plt.title("Correct and incorrect answers per question")
-# plt.savefig('./figures/Figure_2.png')
-# plt.close()
-# sns.catplot(data=df_correct_no_outliers, x="uid", y="time", kind="box", showfliers = False)
-# plt.title("time taken per participant (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_3.png')
-# sns.catplot(data=df_correct_no_outliers, x="style", y="time", kind="box", hue="csexp", showfliers = False)
-# plt.title("time taken per style (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_4.png')
-# sns.catplot(data=df_correct_no_outliers, x="device", y="time", kind="box", hue="csexp", showfliers = False)
-# plt.title("time taken per device (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_5.png')
-# sns.catplot(data=df_correct_no_outliers, y="correctS", x="time", kind="box", showfliers = True)
-# plt.title("time taken per question (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_6.png')
-# sns.catplot(data=df_correct_no_outliers, x="correctI", y="time", kind="box", showfliers = False)
-# plt.title("time taken per correct index (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_7.png')
-# sns.catplot(data=df_correct_no_outliers.query("csexp == 'yes'"), x="style", y="time", kind="box", hue="device", showfliers = False)
-# plt.title("time taken per style by people with CS experience (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_8.png')
-# sns.catplot(data=df_correct_no_outliers.query("csexp == 'no'"), x="style", y="time", kind="box", hue="device", showfliers = False)
-# plt.title("time taken per style by people without CS experience (Correct only, no outliers)")
-# plt.savefig('./figures/Figure_9.png')
+sns.histplot(data=df_no_outliers, x="time")
+plt.title("Distribution of the times recorded (< 20s)")
+plt.savefig('./figures/Figure_0.png')
+plt.close()
+sns.histplot(data=dataframe.query('csexp == "yes"'), x="uid", hue="correct", discrete=True, multiple="stack", palette={True:'green',False:'red'})
+sns.histplot(data=dataframe.query('csexp == "no"'), x="uid", hue="correct", discrete=True, multiple="stack", palette={True:'lime',False:'red'})
+plt.title("Correct and incorrect answers per participant (Green have CS experience)")
+plt.savefig('./figures/Figure_1.png')
+plt.close()
+sns.set(rc={"figure.figsize":(20, 4)})
+sns.histplot(data=dataframe, y="correctS", hue="correct", discrete=True, multiple="stack")
+plt.title("Correct and incorrect answers per question")
+plt.savefig('./figures/Figure_2.png')
+plt.close()
+sns.catplot(data=df_correct_no_outliers, x="uid", y="time", kind="box", showfliers = False)
+plt.title("time taken per participant (Correct only, no outliers)")
+plt.savefig('./figures/Figure_3.png')
+sns.catplot(data=df_correct_no_outliers, x="style", y="time", kind="box", hue="csexp", showfliers = False)
+plt.title("time taken per style (Correct only, no outliers)")
+plt.savefig('./figures/Figure_4.png')
+sns.catplot(data=df_correct_no_outliers, x="device", y="time", kind="box", hue="csexp", showfliers = False)
+plt.title("time taken per device (Correct only, no outliers)")
+plt.savefig('./figures/Figure_5.png')
+sns.catplot(data=df_correct_no_outliers, y="correctS", x="time", kind="box", showfliers = True)
+plt.title("time taken per question (Correct only, no outliers)")
+plt.savefig('./figures/Figure_6.png')
+sns.catplot(data=df_correct_no_outliers, x="correctI", y="time", kind="box", showfliers = False)
+plt.title("time taken per correct index (Correct only, no outliers)")
+plt.savefig('./figures/Figure_7.png')
+sns.catplot(data=df_correct_no_outliers.query("csexp == 'yes'"), x="style", y="time", kind="box", hue="device", showfliers = False)
+plt.title("time taken per style by people with CS experience (Correct only, no outliers)")
+plt.savefig('./figures/Figure_8.png')
+sns.catplot(data=df_correct_no_outliers.query("csexp == 'no'"), x="style", y="time", kind="box", hue="device", showfliers = False)
+plt.title("time taken per style by people without CS experience (Correct only, no outliers)")
+plt.savefig('./figures/Figure_9.png')
 
-# DEVICES = ["mouse", "trackpad", "touch"]
-# CASES = ["kebabcase", "camelcase"]
-# CSEXPS = ["yes", "no"]
-# CORRECTIS = ["== 0","!= 0","any"]
+DEVICES = ["mouse", "trackpad", "touch"]
+CASES = ["kebabcase", "camelcase"]
+CSEXPS = ["yes", "no"]
+CORRECTIS = ["== 0","!= 0","any"]
 
-# df_correct.loc[:,'time'] = np.round(df_correct.loc[:, 'time'])
+df_correct.loc[:,'time'] = np.round(df_correct.loc[:, 'time'])
 
-# print()
-# print(f"\
-# {'%-8s' % 'Device'} \
-# {'%-9s' % 'Style'} \
-# {'%-6s' % 'CSexp'} \
-# {'%-6s' % 'Index'} :      \
-# Minimum    \
-# First quartile            \
-# Median      \
-# Third quartile          \
-# Maximum             \
-# Mean               \
-# STD")
-# print(f"{'-' * 160}")
-# for device in DEVICES:
-#   for case in CASES:
-#     for csexp in CSEXPS:
-#       for correctI in CORRECTIS:
-#         query = f"device == '{device}' and style == '{case}' and csexp == '{csexp}'"
-#         if (correctI != 'any'):
-#           query += f'and correctI {correctI}'
-#         arr = df_correct.query(query)['time']
-#         print(f"{'%-8s' % device} {'%-9s' % case} {'%-6s' % csexp} {'%-6s' % correctI} : \
-#         {'%-9s' % np.min(arr)} \
-#         {'%-9s' % np.quantile(arr, .25)} \
-#         {'%-9s' % np.median(arr)} \
-#         {'%-9s' % np.quantile(arr, .75)} \
-#         {'%-9s' % np.max(arr)} \
-#         {'%-9s' % np.round(np.mean(arr),3)} \
-#         {'%-9s' % np.round(np.std(arr),3)}"
-#         )
+print()
+print(f"\
+{'%-8s' % 'Device'} \
+{'%-9s' % 'Style'} \
+{'%-6s' % 'CSexp'} \
+{'%-6s' % 'Index'} :      \
+Minimum    \
+First quartile            \
+Median      \
+Third quartile          \
+Maximum             \
+Mean               \
+STD")
+print(f"{'-' * 160}")
+for device in DEVICES:
+  for case in CASES:
+    for csexp in CSEXPS:
+      for correctI in CORRECTIS:
+        query = f"device == '{device}' and style == '{case}' and csexp == '{csexp}'"
+        if (correctI != 'any'):
+          query += f'and correctI {correctI}'
+        arr = df_correct.query(query)['time']
+        print(f"{'%-8s' % device} {'%-9s' % case} {'%-6s' % csexp} {'%-6s' % correctI} : \
+        {'%-9s' % np.min(arr)} \
+        {'%-9s' % np.quantile(arr, .25)} \
+        {'%-9s' % np.median(arr)} \
+        {'%-9s' % np.quantile(arr, .75)} \
+        {'%-9s' % np.max(arr)} \
+        {'%-9s' % np.round(np.mean(arr),3)} \
+        {'%-9s' % np.round(np.std(arr),3)}"
+        )
 df_csexp = dataframe.query("csexp == 'yes'")
 mu = np.mean(df_csexp["time"])
 std = np.std(df_csexp["time"])
